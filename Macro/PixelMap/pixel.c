88bcf9c93d1906d4ff5d00287012cd71f0bf5f27
Rowan Decker <Smasher816@gmail.com>
2018-11-30 15:10:08 -0800

    Gamma correction

--- Macro/PixelMap/pixel.c
+++ Macro/PixelMap/pixel.c
@@ -73,7 +73,23 @@ typedef enum PixelTest {
 	PixelTest_XY_Roll,
 } PixelTest;
 
-
+const uint8_t gamma8[] = {
+	0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+	0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+	1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+	2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+	5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+	10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+	17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+	25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+	37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+	51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+	69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+	90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+	115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+	144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+	177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+	215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 };
 
 // ----- Variables -----
 
@@ -1890,13 +1906,16 @@ void Pixel_SecondaryProcessing()
 					}
 
 					val = (uint8_t)((uint16_t*)buf->data)[chan - buf->offset];
+					val = gamma8[val]; // gamma correction
 					val *= profile->pos;
 					val >>= period->end;
 					((uint16_t*)buf->data)[chan - buf->offset] = (uint8_t)val;
 					break;
 				// On hold time
 				case PixelPeriodIndex_On:
-					// Do nothing
+					val = (uint8_t)((uint16_t*)buf->data)[chan - buf->offset];
+					val = gamma8[val]; // gamma correction
+					((uint16_t*)buf->data)[chan - buf->offset] = (uint8_t)val;
 					break;
 				// Off hold time
 				case PixelPeriodIndex_Off:
@@ -1915,6 +1934,7 @@ void Pixel_SecondaryProcessing()
 					if ( prev->start != 0 )
 					{
 						val = (uint8_t)((uint16_t*)buf->data)[chan - buf->offset];
+						val = gamma8[val]; // gamma correction
 						val *= (1 << prev->start) - 1;
 						val >>= prev->end;
 					}
